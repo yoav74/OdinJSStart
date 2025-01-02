7b5994059187daa3c7d7d3ea4c977cc4761d2b4a
yoav74 <yoav74@gmail.com>
2025-01-02 18:39:44 +0200

    formatting

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,23 +1,19 @@
-class Grid_piece {
-    constructor(index) {
-        this.state = "empty";
-        this.Gridindex = index;
-        this.text = Arr[this.Gridindex].firstChild;
-        console.log(this);
-
+class Gridpiece {
+  constructor(index) {
+    this.state = "empty";
+    this.Gridindex = index;
+    this.text = Arr[this.Gridindex].firstChild;
+    console.log(this);
+  }
+  UpdateText() {
+    if (this.state == "empty") {
+      this.text.textContent = null;
+    } else {
+      this.text.textContent = this.state;
     }
-    UpdateText() {
-        if (this.state == "empty") {
-            this.text.textContent = null;
-        }
-        else {
-            this.text.textContent = this.state;
-        }
-        if (this.state == "X")
-            this.text.style.color = "Red";
-        else if (this.state == "O")
-            this.text.style.color = "Blue";
-    }
+    if (this.state == "X") this.text.style.color = "Red";
+    else if (this.state == "O") this.text.style.color = "Blue";
+  }
 }
 
 let currTurn = "O";
@@ -25,85 +21,85 @@ let FilledPlaces = 0;
 const currTurnText = document.querySelector(".Curr_turn");
 const GridDiv = document.querySelectorAll(".grid_item");
 const Arr = Array.from(GridDiv);
-const GridArr = []
+const GridArr = [];
 Arr.forEach((element) => {
-    const newText = document.createElement("span");
-    element.appendChild(newText);
-    const newPiece = new Grid_piece(Arr.indexOf(element));
-    GridArr.push(newPiece);
-})
+  const newText = document.createElement("span");
+  element.appendChild(newText);
+  const newPiece = new Gridpiece(Arr.indexOf(element));
+  GridArr.push(newPiece);
+});
 
 function insert(index) {
-    if (GridArr[index - 1].state == "empty") {
-        GridArr[index - 1].state = `${currTurn}`;
-        GridArr[index - 1].UpdateText();
-        toggleTurn();
-        UpdateUpperText();
-        CheckStatus();
-        FilledPlaces++;
-        if (FilledPlaces >= 9)
-            UpdateWinner("Draw");
-    }
-    else {
-        console.log("This cell is full");
-    }
+  if (GridArr[index - 1].state == "empty") {
+    GridArr[index - 1].state = `${currTurn}`;
+    GridArr[index - 1].UpdateText();
+    toggleTurn();
+    UpdateUpperText();
+    CheckStatus();
+    FilledPlaces++;
+    if (FilledPlaces >= 9) UpdateWinner("Draw");
+  } else {
+    console.log("This cell is full");
+  }
 }
 
 function toggleTurn() {
-    if (currTurn == "X")
-        currTurn = "O";
-    else
-        currTurn = "X";
+  if (currTurn == "X") currTurn = "O";
+  else currTurn = "X";
 }
 
 function UpdateUpperText() {
-    currTurnText.textContent = `Current Turn: ${currTurn}`;
+  currTurnText.textContent = `Current Turn: ${currTurn}`;
 }
 function UpdateWinner(text, winner) {
-    currTurnText.textContent = text;
-    if (winner == "O") {
-        currTurnText.style.color = "Blue";
-        FilledPlaces = 0;
-    }
-    else if (winner == "X") {
-        currTurnText.style.color = "Red";
-        FilledPlaces = 0;
-    }
-    else {
-        currTurnText.style.color = "Yellow";
-    }
-    Arr.forEach(element => {
-        element.onclick = null;
-    })
+  currTurnText.textContent = text;
+  if (winner == "O") {
+    currTurnText.style.color = "Blue";
+    FilledPlaces = 0;
+  } else if (winner == "X") {
+    currTurnText.style.color = "Red";
+    FilledPlaces = 0;
+  } else {
+    currTurnText.style.color = "Yellow";
+  }
+  Arr.forEach((element) => {
+    element.onclick = null;
+  });
 }
 const winningCombinations = [
-    [0, 1, 2], // Top row
-    [3, 4, 5], // Middle row
-    [6, 7, 8], // Bottom row
-    [0, 3, 6], // Left column
-    [1, 4, 7], // Middle column
-    [2, 5, 8], // Right column
-    [0, 4, 8], // Diagonal from top-left
-    [2, 4, 6]  // Diagonal from top-right
+  [0, 1, 2], // Top row
+  [3, 4, 5], // Middle row
+  [6, 7, 8], // Bottom row
+  [0, 3, 6], // Left column
+  [1, 4, 7], // Middle column
+  [2, 5, 8], // Right column
+  [0, 4, 8], // Diagonal from top-left
+  [2, 4, 6], // Diagonal from top-right
 ];
 
 function CheckStatus() {
-    for (const combination of winningCombinations) {
-        const [a, b, c] = combination;
-        if (GridArr[a].state != "empty" && GridArr[a].state === GridArr[b].state && GridArr[a].state === GridArr[c].state) {
-            UpdateWinner("Winner : " + GridArr[a].state, GridArr[a].state) // Return the winner ('X' or 'O')
-        }
+  for (const combination of winningCombinations) {
+    const [a, b, c] = combination;
+    if (
+      GridArr[a].state != "empty" &&
+      GridArr[a].state === GridArr[b].state &&
+      GridArr[a].state === GridArr[c].state
+    ) {
+      UpdateWinner("Winner : " + GridArr[a].state, GridArr[a].state); // Return the winner ('X' or 'O')
     }
+  }
 }
 
 function reset() {
-    GridArr.forEach(element => {
-        element.state = "empty";
-        element.UpdateText();
-        Arr[GridArr.indexOf(element)].onclick = function () { insert(GridArr.indexOf(element) + 1) };
-    })
-    currTurn = "O";
-    UpdateUpperText();
-    currTurnText.style.color = "Black";
-    FilledPlaces = 0;
+  GridArr.forEach((element) => {
+    element.state = "empty";
+    element.UpdateText();
+    Arr[GridArr.indexOf(element)].onclick = function () {
+      insert(GridArr.indexOf(element) + 1);
+    };
+  });
+  currTurn = "O";
+  UpdateUpperText();
+  currTurnText.style.color = "Black";
+  FilledPlaces = 0;
 }
